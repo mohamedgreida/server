c2a285d110a7765f9b88beeb1ce8349b483c91cc
Mohamed Greida <Mohamed_Greida@aucegypt.edu>
2026-03-31 14:04:41 +0200

    Server

---
+++ server.cpp
@@ -0,0 +1,26 @@
+#include <boost/asio.hpp>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string(){
+	std::time_t now = std::time(0);
+	return std::ctime(&now);
+}
+int main(){
+	boost::asio::io_context io_context;
+
+	tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),13));
+
+	while(true){
+		tcp::socket socket(io_context);
+		acceptor.accept(socket);
+		std::string message =  make_daytime_string();
+		boost::system::error_code ignored_error;
+		boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+
+	}
+	return 0;
+}

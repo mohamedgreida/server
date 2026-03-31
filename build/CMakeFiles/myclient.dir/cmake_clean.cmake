38f830bd099f8f8c9088ebfb65d74eae0cbc9cfa
Mohamed Greida <Mohamed_Greida@aucegypt.edu>
2026-03-31 14:18:54 +0200

    Added client

---
+++ build/CMakeFiles/myclient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myclient.dir/client.cpp.o"
+  "CMakeFiles/myclient.dir/client.cpp.o.d"
+  "myclient"
+  "myclient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
